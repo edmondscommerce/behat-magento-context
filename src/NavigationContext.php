8ce2c29ed4480e2d7ef8a41b5d9b8aea346d6106
Dan <dan@edmondscommerce.co.uk>
2016-01-21 11:29:51 +0000

    Fixed incorrect class definition

--- src/NavigationContext.php
+++ src/NavigationContext.php
@@ -5,7 +5,7 @@ use Behat\Behat\Context\SnippetAcceptingContext;
 use Behat\MinkExtension\Context\RawMinkContext;
 use Exception;
 
-class BasicPageNavigationContext extends RawMinkContext implements Context, SnippetAcceptingContext
+class NavigationContext extends RawMinkContext implements Context, SnippetAcceptingContext
 {
     /**
      * @When /^(?:|I )click on the element "([^"]*)"$/
