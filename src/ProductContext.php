9405bfef901ff6125754a08871679ec532ed0a87
Dan <dan@edmondscommerce.co.uk>
2016-01-21 15:35:28 +0000

    Added product option context

--- src/ProductContext.php
+++ src/ProductContext.php
@@ -1,6 +1,34 @@
 <?php namespace EdmondsCommerce\BehatMagentoOneContext;
 
+use Behat\Mink\Element\NodeElement;
+use InvalidArgumentException;
+
 class ProductContext extends AbstractMagentoContext
 {
+    /**
+     * Choose an option for a configurable product
+     *
+     * @Then /^I choose product option "([^"]*)"$/
+     */
+    public function iChooseProductOptionFor($option)
+    {
+        //Get the container
+        $session = $this->getSession();
+        $optionContainer = $session->getPage()->findById('product-options-wrapper');
 
+        /** @var NodeElement[] $values */
+        $values = $optionContainer->findAll('xpath', 'dl/dd/div/ul/li/a');
+
+        foreach ($values as $v)
+        {
+            if ($v->getAttribute('name') == $option)
+            {
+                //Chose the option
+                $v->click();
+                return;
+            }
+        }
+
+        throw new InvalidArgumentException(sprintf('Could not find a product option: "%s"', $option));
+    }
 }
